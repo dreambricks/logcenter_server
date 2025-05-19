ae330060527036dce5f09e533a0114a370bd4e05
Julio Cesar Balbino de Morais <julio4110@hotmail.com>
2025-05-19 13:14:01 -0300

    feat: add owner to project

--- project.py
+++ project.py
@@ -8,38 +8,41 @@ project = Blueprint('project', __name__)
 
 
 class Project:
-    def __init__(self, name, createdAt):
+    def __init__(self, name, createdAt, owner):
         self.name = name
         self.createdAt = createdAt
+        self.owner = owner
 
     def save(self):
         collection = db['project']
         data = {
             'name': self.name,
-            'createdAt': self.createdAt
+            'createdAt': self.createdAt,
+            'owner': self.owner
         }
         result = collection.insert_one(data)
         return result.inserted_id
 
     def __str__(self):
-        return f"{self.name} - {self.createdAt}"
+        return f"{self.name} - {self.createdAt} - {self.owner}"
 
 
 @project.route('/project/upload', methods=['POST'])
 def create():
+    name = request.form.get('name')
+    owner = request.form.get('owner')
+
+    if not name or not owner:
+        return jsonify({'error': 'Name and owner are required fields'}), 400
+
     data_hora_atual = datetime.now()
-
     data_hora_formatada = data_hora_atual.strftime("%Y-%m-%dT%H:%M:%SZ")
-
     createdAt = datetime.strptime(data_hora_formatada, "%Y-%m-%dT%H:%M:%SZ")
 
-    name = request.form.get('name')
-
-    prj = Project(name, createdAt)
+    prj = Project(name, createdAt, owner)
     prj.save()
 
     return '', 200
-
 
 @project.route('/project')
 def find_all():
